7785182370af06111cfc0fe8105f4fb9a98f3fcc
HUAWEI15D\nafte <naf.ter@mail.ru>
2022-04-13 21:19:09 +0300

    13.04.2022_1

--- js/components/StoreHouse.js
+++ js/components/StoreHouse.js
@@ -1,11 +1,19 @@
 'use strict'
 class StoreHouse extends WindowClass {
+	#transport;
 	constructor ( application, parent ) {
 		super( application, parent );
+		this.#transport = this.Application.Transport;
 		this.Title = "Склад. Журнал документов";
-		this.Show();
+		this.#GetDicts();
+		this.#GetJournal();
+		// this.Show();
 	}
 
+	#GetJournal() {
+		console.log("++++");
+		this.#transport.Get({com: 'skyline.apps.adapters', subcom: 'appApi', data: {action: 'getStoreJournal'}, hash: this.Hash});
+	}
 	// get Title() {return this.#wTitle;}
 
 	Commands ( packet ) {
@@ -15,7 +23,9 @@ class StoreHouse extends WindowClass {
 				switch ( packet.subcom ) {
 					case 'appApi':
 						switch ( packet.data.action ) {
-
+							case "getStoreJournal":
+								console.log("ppppp=> ", packet);
+							break;
 						}
 					break;
 				}
